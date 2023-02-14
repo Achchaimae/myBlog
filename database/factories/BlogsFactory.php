8372326ccd607340bade315b734db9c45e5919f2
achchaimae <achchaimae1@gmail.com>
2023-02-14 12:11:16 +0100

    db 📌

---
+++ database/factories/BlogsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blogs>
+ */
+class BlogsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'tags' => $this->faker->sentence(),
+            'content' => $this->faker->paragraph(), 
+            'category' => $this->faker->sentence(),
+            'comments' => $this->faker->sentence(),
+        ];
+    }
+}
